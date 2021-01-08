9d2c9860115fb9787e28706a07ba3718d2f2bf9f
Elias Hussen <elsnyc@gmail.com>
2021-01-08 11:06:44 -0500

    Finalizing onnx.py for testing in colab

--- onnx.py
+++ onnx.py
@@ -36,7 +36,7 @@ class TorchToONNX:
         """
          Converts torch model into ONNX + optimizes it into a small onnx file
            - requires an empty directory
-           -
+           - currently model config is limited to bert (768 hidden size x 12 attentions)
         :return:
         """
         if os.path.exists(self.onnx_model_local_dir):
@@ -64,7 +64,11 @@ class TorchToONNX:
         optimized_model.save_model_to_file(str(self.optimized_model_path))
 
     def create_model_for_provider(self, provider: str = 'CPUExecutionProvider') -> InferenceSession:
-
+        """
+        prepares optimized model for inference
+        :param provider:
+        :return:
+        """
         assert provider in get_all_providers(), f"provider {provider} not found, {get_all_providers()}"
 
         # Few properties than might have an impact on performances (provided by MS)
@@ -74,7 +78,7 @@ class TorchToONNX:
         # Load the model as a graph and prepare the CPU backend
         return InferenceSession(str(self.optimized_model_path), options, providers=[provider])
 
-    def run_inference(self, text, proba=True):
+    def run_inference(self, text, tokenizer, proba=True):
         model_inputs = tokenizer(
             text,
             padding=True,
@@ -88,16 +92,56 @@ class TorchToONNX:
 
         return softmax(logits[0], axis=1)
 
-#
-# tokenizer = DistilBertTokenizerFast.from_pretrained('distilbert-base-uncased')
-#
-# sentence = 'This is some medical device text that needs to be manufactured in china'
-#
-# tmp = Path('/content/drive/MyDrive/elite/elitelearn/models/medical-device-timeline-classifier/pytorch_model.bin')
-# omp = Path('/content/drive/MyDrive/elite/elitelearn/models/medical-device-timeline-classifier/onnx')
-#
-# tto = TorchToONNX(
-#     torch_model_path=tmp,
-#     onnx_model_dir=omp,
-#     tokenizer=tokenizer
-# )
+
+"""
+    Below code demonstrates how this is to be used
+"""
+
+'''
+from pathlib import Path 
+from transformers import DistilBertTokenizerFast
+
+tokenizer = DistilBertTokenizerFast.from_pretrained('distilbert-base-uncased')
+
+text = 'This is some medical device text that needs to be manufactured in china'
+
+tmp = Path('/content/drive/MyDrive/elite/elitelearn/models/medical-device-timeline-classifier/pytorch_model.bin')
+omp = Path('/content/drive/MyDrive/elite/elitelearn/models/medical-device-timeline-classifier/onnx')
+
+tto = TorchToONNX(
+    torch_model_path=tmp,
+    onnx_model_dir=omp,
+    tokenizer=tokenizer
+)
+
+tto.convert_torch_to_onnx()
+'''
+
+'''
+How to run inference
+
+from scipy.special import softmax
+
+sentence = ["""The endurant II bifurcated stent grafts are being offered in 31 configurations based on 
+various combinations of the proximal main body diameters, covered lengths of the bifurcated stent graft, 
+and the distal diameter of the ipsilateral leg. We are introducing a shorter ipsilateral leg and 
+fixed covered length of 103 mm limits the number of endurant IIs configuration to five; 
+which will aid the physician with inventory management"""]
+
+
+model_inputs = tokenizer(
+    sentence,
+    padding=True,
+    add_special_tokens=True,
+    max_length=64,
+    return_tensors='pt'
+)
+
+inputs_onnx = {k: v.cpu().detach().numpy() for k, v in model_inputs.items()}
+
+cpum = tto.create_model_for_provider("CPUExecutionProvider")
+# model_inputs = tokenizer.encode_plus(["click to see what's in store"], return_tensors="pt", return_token_type_ids=False)
+res = cpum.run(None, inputs_onnx)
+probs = softmax(res[0], axis=1)
+probs
+'''
